e8ab97d1d28be2a23081e7390cda290604d7bbc0
Ahmad Arif <ahmad.arif019@gmail.com>
2018-12-25 16:15:58 +0700

    add authentication library

---
+++ src/class/response.ts
@@ -0,0 +1,4 @@
+export default class Response<T> {
+  message: string;
+  data: T;
+}
--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from '../services/app.service';
+import Response from '../class/response';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): Response<string[]> {
+    const res = new Response<string[]>();
+    res.message = 'Ok';
+    res.data = [...this.appService.getHello()];
+    return res;
   }
 }
