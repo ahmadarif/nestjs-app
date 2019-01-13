f68cbe700fef13682542fcd0e388fc70014fcef0
Ahmad Arif <ahmad.arif019@gmail.com>
2019-01-14 06:06:54 +0700

    add active record style in user service auth module

--- src/modules/auth/user.service.ts
+++ src/modules/auth/user.service.ts
@@ -6,9 +6,10 @@ import { InjectRepository } from "@nestjs/typeorm";
 @Injectable()
 export class UserService {
   constructor(
-    // private readonly connection: Connection,
+    private readonly connection: Connection,
     @InjectRepository(User) private readonly userRepository: Repository<User>
   ) {}
+
   async findOneByToken(token: string): Promise<any> {
     // return await this.connection
     //   .getRepository(User)
@@ -17,8 +18,13 @@ export class UserService {
     //   .leftJoinAndSelect('user.profile', 'profile')
     //   .getOne();
 
-    return await this.userRepository.findOne({
-      where: {name: token},
+    // return await this.userRepository.findOne({
+    //   where: {name: token},
+    //   relations: ['profile']
+    // });
+
+    return await User.findOne({
+      where: { name: token },
       relations: ['profile']
     });
   }
